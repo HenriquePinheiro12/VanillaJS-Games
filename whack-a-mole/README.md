baacc739a6e77718dc6870d90e8e99d518ad4bde
HenriquePinheiro <pinheirohsp@gmail.com>
2022-03-01 20:23:30 -0300

    whack a mole done!

---
+++ whack-a-mole/README.md
@@ -0,0 +1,5 @@
+# Vanilla JS Whack a Mole
+
+## What I learned
+
+- [X] Working with setInterval and clearInterval
---
+++ whack-a-mole/script.js
@@ -0,0 +1,53 @@
+const $squares = document.querySelectorAll('.square')
+const $score = document.querySelector('.score')
+const $timeLeft = document.querySelector('.time-left')
+
+let count = 60;
+let score = 0;
+
+// references the id of the current mol. Used in whackMole
+let currentMoleId;
+
+const countDown = () => {
+    count--;
+    $timeLeft.textContent = count
+
+    if(count === 0){ // ends up the game
+        alert(`Game\´s over! Your score: ${score}`)
+        clearInterval(moleTimer) //stops moles generating
+        clearInterval(countTimer) //stops countdown
+        createResetBtn()
+    }
+}; const countTimer = setInterval(countDown, 1000)
+
+// generates randomly the mols
+const showMole = () => {
+    // removes the current mole
+    $squares.forEach(square => square.classList.remove('mole'))
+    
+    // sets a new mole randomly
+    const targetSquare = $squares[Math.floor(Math.random() * 9)] 
+    targetSquare.classList.add('mole')
+
+    currentMoleId = targetSquare.id
+}; const moleTimer = setInterval(showMole, 500)
+
+// hits the mol and increase the score
+const whackMole = e => {
+    if(e.target.id !== currentMoleId) return
+    score++;
+    $score.textContent = score
+    currentMoleId = null
+}; $squares.forEach(square => {
+    square.addEventListener('mouseover', whackMole)
+})
+
+// creates a button to refresh when time´s over
+const createResetBtn = () => {
+    const $footer = document.querySelector('.footer')
+    const button = document.createElement('a')
+    button.classList.add('reset')
+    button.textContent = 'reset';
+    button.setAttribute('href', 'index.html')
+    $footer.append(button)
+}
