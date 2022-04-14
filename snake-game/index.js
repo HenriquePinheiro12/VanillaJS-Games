314bc940383d071851ff78d5370b5d70dcced0ce
HenriquePinheiro <pinheirohsp@gmail.com>
2022-04-14 20:35:47 -0300

    snake game done!

--- snake-game/index.js
+++ snake-game/index.js
@@ -5,7 +5,9 @@ const snake = {
     length : 3, // score
     direction : 1,
     speed : 1000,
-    body : [13, 12, 11]
+    body : [13, 12, 11],
+    score : 0,
+    colisioned : false
 }
 let apples = [];
 let gameOver = false
@@ -24,17 +26,19 @@ const startGame = () => {
     snake.direction = 1;
     snake.speed = 1000
     snake.body = [13, 12, 11]
+    snake.score = 0
+    snake.colisioned = false
 
-
-    $squares.forEach(sqr => sqr.classList.remove('snake'))
+    apples = []
+    $squares.forEach(sqr => sqr.classList.remove('snake', 'apple', 'snake-head'))
     for(i of snake.body) $squares[i].classList.add('snake')
-
+    $squares[snake.body[0]].classList.add('snake-head')
 
     setTimeout(move, snake.speed)
     setTimeout(generateApple, snake.speed * 1.5)
 }
 
-const move = () => {7
+const move = () => {
     console.log(snake.speed)
     const {body, direction} = snake
 
@@ -43,14 +47,20 @@ const move = () => {7
         body[0] < 10 && direction === -10 ||
         (body[0] + 1) % 10 === 0 && direction === 1 ||
         body[0] >= 90 && direction === 10 ||
-        body[0] % 10 == 0 && direction === -1
+        body[0] % 10 == 0 && direction === -1  ||
+        // handle snake body colision
+        // snake.body[0] === snake.body[i] | i > 0
+        body.includes(body[0] + direction)
     ) return handleColision()
 
+    // moving
     const removedEl = body.pop()
     body.unshift(body[0] + direction)
 
+    // rendering
     $squares[removedEl].classList.remove('snake')
-    $squares[body[0]].classList.add('snake')
+    $squares[body[0]].classList.add('snake', 'snake-head')
+    $squares[body[1]].classList.remove('snake-head')
 
 
     if(apples.includes(body[0])){
@@ -58,12 +68,22 @@ const move = () => {7
         body.push(removedEl)
         apples.splice(apples.indexOf(body[0]), 1)
         $squares[body[0]].classList.remove('apple')
+        snake.speed = 
+            snake.speed > 700 ? 
+            snake.speed * 0.7 : snake.speed > 200 ? 
+            snake.speed * 0.8 : snake.speed > 150  ? 
+            snake.speed * 0.95 : snake.speed // ends up speed at 150ms
+        snake.score++
+        $score.innerText = snake.score
     }
     
     setTimeout(move, snake.speed)
 }
 
 const generateApple = () => {
+    // stop apple generation when crash
+    if(snake.colisioned) return
+
     let newApple;
     do{
         newApple = Math.floor(Math.random() * 100)
@@ -72,7 +92,7 @@ const generateApple = () => {
     apples.push(newApple)
     $squares[newApple].classList.add('apple')
 
-    setTimeout(generateApple, snake.speed * 2)
+    setTimeout(generateApple, snake.speed * 5)
 }
 
 const changeSnakeDirection = ({keyCode}) => {
@@ -102,13 +122,11 @@ const changeSnakeDirection = ({keyCode}) => {
             snake.direction = 10
             break;
     }
-
-    console.log(snake.direction)
 }
 
 const handleColision = () => {
-    
-    alert(`Game over! Score: ${snake.length}`)
+    alert(`Game over! Score: ${snake.score}`)
+    snake.colisioned = true
 }
 
 $start.addEventListener('click', startGame)
